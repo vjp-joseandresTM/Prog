cdb63420f19e126b1c491748315f93b4863dcde7
1DAM - Jose Tarrero Mendez <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-10-10 18:50:38 +0200

    Añadido ejercicio 12

---
+++ T4/Ejercicio12/src/ejercicio12/Ejercicio12.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio12;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 12 ;
+        do {            
+            System.out.println(i);
+            i += 2;
+        } while (i<=133);
+    }
+    
+}
