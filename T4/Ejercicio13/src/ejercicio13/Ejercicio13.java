e7405379e3c2e0ef45d3b487f60109ae6344aff1
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-10-11 19:16:00 +0200

    Ejercicio13 completado

--- T4/Ejercicio13/src/ejercicio13/Ejercicio13.java
+++ T4/Ejercicio13/src/ejercicio13/Ejercicio13.java
@@ -2,27 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio16;
+package ejercicio13;
 
 /**
  *
  * @author DAM
  */
-public class Ejercicio16 {
+public class Ejercicio13 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int i = 21 ;
-       int contador = 0;
-        do {            
+        int i = 12;
+        
+        while(i <= 133 ){
             System.out.println(i);
             i += 2;
-            contador += 1;  // Esta variable sirve como un contador, pues en cada vuelta se suma 1
-        } while (i<=160);
-        
-        System.out.println("EL NUMERO DE NUMEROS IMPARES IMPRESOS EN PANTALLA ES DE " + contador); 
-
+        }
+   
     }
+    
 }
