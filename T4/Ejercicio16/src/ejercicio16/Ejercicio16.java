88fda6894dd63dcd304c44128d2dbe80cd1a835a
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-10-11 18:50:11 +0200

    Ejercicio 16 completado

---
+++ T4/Ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio16;
+
+/**
+ *
+ * @author DAM
+ */
+public class Ejercicio16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int i = 21 ;
+       int contador = 0;
+        do {            
+            System.out.println(i);
+            i += 2;
+            contador += 1;
+        } while (i<=160);
+        
+        System.out.println("EL NUMERO DE NUMEROS IMPARES IMPRESOS EN PANTALLA ES DE " + contador); 
+
+    }
+}
