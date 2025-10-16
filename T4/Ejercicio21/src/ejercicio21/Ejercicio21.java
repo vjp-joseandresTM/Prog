c5b0b495d37c274f4ca4150a386e10e8b44c6a0b
1DAM - Jose Tarrero Mendez <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-10-16 17:04:24 +0200

    modificado

--- T4/Ejercicio21/src/ejercicio21/Ejercicio21.java
+++ T4/Ejercicio21/src/ejercicio21/Ejercicio21.java
@@ -2,25 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio14;
+package ejercicio21;
 
 /**
  *
- * @author DAM
+ * @author alumno
  */
-public class Ejercicio14 {
+public class Ejercicio21 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int i = 2;
-        
-        while(i<=200){
-            System.out.println(i);
-            i += 2;
-        } //Uso un bucle while pues no se el numero de veces que debo hacer el bucle y porque resume mucho mas el codigo.
+        // TODO code application logic here
     }
     
 }
- 
