880c550c92b23695c30e979cc42003287f35bf52
1DAM - Jose Tarrero Mendez <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-10-17 16:38:22 +0200

    Ejercicio 21

--- T4/Ejercicio21/src/ejercicio21/Ejercicio21.java
+++ T4/Ejercicio21/src/ejercicio21/Ejercicio21.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio21;
-
+import java.util.Scanner;
 /**
  *
  * @author alumno
@@ -14,7 +14,24 @@ public class Ejercicio21 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    Scanner entrada  = new Scanner(System.in);
+    int numero1;
+    int numero2;
+    int resultado;
+    
+    System.out.print("Introduce el primer numero: ");
+    numero1 = entrada.nextInt();
+    System.out.print("Introduce el primer numero: ");
+    numero2 = entrada.nextInt();
+    
+        try {
+            resultado = numero1 / numero2 ;
+            System.out.println("El resultado de dividir los numero fue " + resultado);
+            
+        } catch (ArithmeticException e) {
+            System.err.println("ERROR: No se puede dividir entre 0");
+        }
+    
     }
     
 }
