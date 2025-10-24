4db824ea5119a026b1a9702803bc85617360e982
1DAM - Jose Tarrero Mendez <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-10-24 20:30:12 +0200

    añadido

---
+++ T4/Ejercicio 7/src/ejercicio/pkg7/Ejercicio7.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio.pkg7;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+        
+        System.out.println("Introduce el primer numero: ");
+        num1 = sc.nextInt();
+        System.out.println("Introduce el segundo numero: ");
+        num2 = sc.nextInt();
+        System.out.println("Introduce el tercer numero: ");
+        num3 = sc.nextInt();
+        System.out.println("Introduce el cuarto numero: ");
+        num4 = sc.nextInt();
+        Vasos(num1,num2,num3,num4);
+        
+                    
+    }
+    
+    public static void Vasos(){
+    
+    }
+    
+}
