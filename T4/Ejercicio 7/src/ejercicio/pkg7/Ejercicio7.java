ab23fd3513c1690487badde4836b193986660a82
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-10-30 10:55:40 +0100

    Comentarios añadidos

--- T4/Ejercicio 7/src/ejercicio/pkg7/Ejercicio7.java
+++ T4/Ejercicio 7/src/ejercicio/pkg7/Ejercicio7.java
@@ -16,12 +16,12 @@ public class Ejercicio7 {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1;
+        int num1;//Declaramos las variables
         int num2;
         int num3;
         int num4;
         
-        System.out.println("Introduce el primer numero: ");
+        System.out.println("Introduce el primer numero: ");//Le pedimos al usuario que introduzca los numeros.
         num1 = sc.nextInt();
         System.out.println("Introduce el segundo numero: ");
         num2 = sc.nextInt();
@@ -29,16 +29,16 @@ public class Ejercicio7 {
         num3 = sc.nextInt();
         System.out.println("Introduce el cuarto numero: ");
         num4 = sc.nextInt();
-        Vasos(num1,num2,num3,num4);
+        Vasos(num1,num2,num3,num4);//Pasamos los datos a Vasos para proceder a operar con ellos.
         
                     
     }
     
     public static void Vasos(int num1, int num2, int num3, int num4){
-        int cambio;
+        int cambio; //Declaro la variabel Cambio la cual jugara como el vaso 
         int i;
         for(i = 1; i<=3; i++){
-            if(num1>num2){
+            if(num1>num2){ //Compienzo a ccmpararr los numeros para reordenarlos .
              cambio = num1;
              num1 = num2;
              num2 = cambio;
