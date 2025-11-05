f8845130cad26cc23f2ac787876f08fc39f488fc
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-11-05 09:57:43 +0100

    añadidos

--- T4/Ejercicicio 17/src/ejercicicio/pkg17/Ejercicicio17.java
+++ T4/Ejercicicio 17/src/ejercicicio/pkg17/Ejercicicio17.java
@@ -14,8 +14,8 @@ public class Ejercicicio17 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      char letraGenerada = generarLetra();
-       
+      
+      juego(); 
     }
     public static char pedirLetra(){
         Scanner sc = new Scanner(System.in);
@@ -25,11 +25,33 @@ public class Ejercicicio17 {
         return letraUser;
         
         
+        
     }
     public static char generarLetra(){
         int numero = (int) (Math.random()*(122-97+1))+ 97; 
         return (char)numero;
        
+    }
+    
+    public static char juego(){
+        char generado = generarLetra();
+        char solicitada;
+        int intentos = 0;
+        do {
+            solicitada = pedirLetra();
+
+            if (solicitada < generado) {
+                intentos++;
+                System.out.println("La letra es más alta en el abecedario");
+            } else if (solicitada > generado) {
+                intentos++;
+                System.out.println("La letra es más baja en el abecedario");
+            }
+
+        } while (solicitada != generado);
+
+        System.out.println("¡DIN DIN DIN! La letra era:  " + generado + " Los intentos fueron :" + intentos);
+        return generado;
     }
     
 
