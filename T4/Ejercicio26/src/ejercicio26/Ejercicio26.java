d2a3bc0e089056137626876458403d3b46323752
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-11-06 20:44:17 +0100

    Comenzado ejercicio 26

---
+++ T4/Ejercicio26/src/ejercicio26/Ejercicio26.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio26;
+import java.util.Scanner;
+/**
+ *
+ * @author Jose
+ */
+public class Ejercicio26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static int numeros(){
+    Scanner sc = new Scanner (System.in);
+        int num;
+        System.out.println("Introduce un numero: ");
+        num = sc.nextInt();
+        return num;
+    }
+    
+    public static boolean esPrimo(int num){
+        int i;
+        while(){
+            
+        }
+    }
+}
