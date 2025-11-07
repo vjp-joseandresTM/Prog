92cee6cd6d66698486fe322f43f40426d610d79c
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-11-07 12:08:48 +0100

    añadido 22

---
+++ T4/Ejercicio22/src/ejercicio22/Ejercicio22.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio22;
+
+/**
+ *
+ * @author DAM
+ */
+public class Ejercicio22 {
+public static final double PI = Math.PI;
+public static final double EULER  = Math.exp(4);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mostrarCons();
+    }
+    
+    public static void mostrarCons(){
+        System.out.println("La constante PI expresa: " + PI);
+        System.out.println("La constante de Euler con 4 expresa: " + EULER);
+       
+        
+    }
+    
+}
