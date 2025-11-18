49596683b10d001fe679606cdcd3ad0c2144f590
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-11-18 11:54:00 +0100

    Ejercicio 15 finalizado

--- T5/Ejercicio15/src/ejercicio15/Ejercicio15.java
+++ T5/Ejercicio15/src/ejercicio15/Ejercicio15.java
@@ -17,14 +17,16 @@ public class Ejercicio15 {
         Direccion dir1 = new Direccion("Tenenrias", 1, "Seattle",23);
         Empleado em1 = new Empleado("JoseJoaquin", 80, dir1.getCalle(), dir1.getNpiso(), dir1.getCiudad(), dir1.getNcalle() );
         em1.mostrarEmpleado();
-        
+        System.out.println("");
+        System.out.println("");
         Direccion dir2 = new Direccion("Las cabezas", 70, "Jaraíz", 2);
-        Empleado em2 = new Empleado("Roberto", 80, dir1.getCalle(), dir1.getNpiso(), dir1.getCiudad(), dir1.getNcalle() );
+        Empleado em2 = new Empleado("Roberto", 80, dir2.getCalle(), dir2.getNpiso(), dir2.getCiudad(), dir2.getNcalle() );
         em2.mostrarEmpleado();
-        
-        Direccion dir3 = new Direccion("Tenenrias", 1, "Seattle",23);
-        Empleado em3 = new Empleado("JoseJoaquin", 80, dir1.getCalle(), dir1.getNpiso(), dir1.getCiudad(), dir1.getNcalle() );
-        em2.mostrarEmpleado();
+        System.out.println("");
+        System.out.println("");
+        Direccion dir3 = new Direccion("La Esquina", 4, "Conil",23);
+        Empleado em3 = new Empleado("Enrique", 80, dir3.getCalle(), dir3.getNpiso(), dir3.getCiudad(), dir3.getNcalle() );
+        em3.mostrarEmpleado();
     }
     
 }
