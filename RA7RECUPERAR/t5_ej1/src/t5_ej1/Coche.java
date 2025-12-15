cd0d2aa1224b054203682dbc9eb2638d173205bc
1DAM - Jose Tarrero Mendez <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2025-12-15 20:49:31 +0100

    añadido

---
+++ RA7RECUPERAR/t5_ej1/src/t5_ej1/Coche.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t5_ej1;
+
+/**
+ *
+ * @author alumno
+ */
+public class Coche {
+    String marca;
+    String modelo;
+    String color;
+    int velocidad;
+    boolean motorEncendido;
+    
+    public void establecerMarca(){
+        this.marca = marca;
+    }
+        public void establecerModelo(){
+        this.modelo = modelo;
+        
+    }
+    public void establecerColor(){
+        this.color = color;
+    }
+    
+    public void arrancarCoche(){
+        motorEncendido = true;
+    }
+    
+    p
+}
