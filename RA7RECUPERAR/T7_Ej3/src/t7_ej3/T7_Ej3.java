81307d5edd5d1d32dd657e725fbdf073e4d72fc8
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2026-01-09 13:04:49 +0100

    Añadidos

---
+++ RA7RECUPERAR/T7_Ej3/src/t7_ej3/T7_Ej3.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t7_ej3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM
+ */
+public class T7_Ej3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //Pedimos al usuario un numero de lognitud en el main
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce la longitud (máx 10): ");
+        int longitud = sc.nextInt();
+
+        if (longitud > 0 && longitud <= 10) {
+            int[] numero = new int[longitud];
+            genRand(numero);
+            mostrarNum(numero);
+        } else {
+            System.out.println("La longitud debe ser entre 1 y 10.");
+        }
+    }
+    //Generamos aleatorios
+    public static void genRand(int[] numero) {
+        for (int i = 0; i < numero.length; i++) {
+            numero[i] = (int) (Math.random() * 10) + 1;
+        }
+    }
+    //Imprimimos los numeros 
+    public static void mostrarNum(int[] numero) {
+        for (int i = 0; i < numero.length; i++) {
+            System.out.println(numero[i]);
+        }
+    }
+
+}
