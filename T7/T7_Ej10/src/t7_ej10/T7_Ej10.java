1a9c52daee6e740225aaaefb0da096d60cb84fc4
1DAM - Jose Tarrero Mendez <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2026-01-13 15:36:22 +0100

    comentarios del anterior commit arreglados

--- T7/T7_Ej10/src/t7_ej10/T7_Ej10.java
+++ T7/T7_Ej10/src/t7_ej10/T7_Ej10.java
@@ -13,6 +13,8 @@ public class T7_Ej10 {
     /**
      * @param args the command line arguments
      */
+    
+   //Llamo a los metodos, y declaro una variable.
     public static void main(String[] args) {
         int[] numeros = new int[10];
         rellenarArray(numeros);
@@ -20,20 +22,20 @@ public class T7_Ej10 {
         ordenarZero(numeros);
         mostrarArray(numeros);
     }
-    
+    //Aqui el metodo rellena el array con numeros aleatorios usando el Math.random.
     public static void rellenarArray(int[] numeros){
         for(int i = 0; i<10; i++){
             numeros[i] = (int)(Math.random()*8)+1;
            
         }
     }
-    
+    //El metodo recorre los indices del array y muestra el contenido de los dichos.
     public static void mostrarArray(int[] numeros){
         for(int i = 0; i<10;i++){
             System.out.println("Numero: " + numeros[i]);
         }
     }
-    
+    //Me ayduo en este metodo de dos For para comprobar cual esta repetido y posteriormente suprimo el 0.
     public static void ordenarZero(int[] numeros){
         for (int i = 0; i < numeros.length; i++) {
             for(int j = i+1; j<numeros.length;j++){
