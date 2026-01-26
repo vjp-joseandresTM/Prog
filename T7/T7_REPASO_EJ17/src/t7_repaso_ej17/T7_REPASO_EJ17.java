9fdfafaa5a6b015ca36998b74b71d2180342dfb9
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2026-01-26 12:59:45 +0100

    add

---
+++ T7/T7_REPASO_EJ17/src/t7_repaso_ej17/T7_REPASO_EJ17.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t7_repaso_ej17;
+
+/**
+ *
+ * @author DAM
+ */
+public class T7_REPASO_EJ17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        genNum(numeros);
+        mostrador(numeros);
+    }
+    
+    public static void genNum(int[] numeros){
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (int)(Math.random()*10);
+        }
+ 
+    }
+    
+    public static void mostrador(int[] numeros){
+        System.out.println("Numeros desordenados: ");
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i] == numeros[9]){
+                System.out.print(numeros[i]);
+            }else{
+                System.out.print(numeros[i]+", ");
+            }
+        }
+        System.out.println("\n");
+    }
+    
+    public static void organizador(int[] numeros){
+        int aux;
+        for (int i = 0; i < numeros.length; i++) {
+            for(int j = 0; i < numeros.length;j++){
+                if(numeros[i]>numeros[i+1]){
+                    
+                }
+            }
+        }
+    }
+    
+}
