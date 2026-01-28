e7e8f5c0d5f0be81cf79d551cd4a688401fd42bc
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2026-01-28 12:26:57 +0100

    Añadidos

--- T7/T8_EJ6/src/t8_ej6/Empleados.java
+++ T7/T8_EJ6/src/t8_ej6/Empleados.java
@@ -9,21 +9,68 @@ package t8_ej6;
  * @author DAM
  */
 public class Empleados {
+
     private String nombre;
     private int horasTrabajadas;
     private double tarifaXhora;
-    
+
     //Constructores
-    public Empleados(){
+    public Empleados() {
         this.nombre = "";
         this.horasTrabajadas = 0;
         this.tarifaXhora = 0;
     }
-    
-   public Empleados(String nombre, int horasTrabajadas, double tarifaXhora){
-       this.nombre = nombre;
-       this.horasTrabajadas = horasTrabajadas;
-       this.tarifaXhora = tarifaXhora;
-   }
-   
+
+    public Empleados(String nombre, int horasTrabajadas, double tarifaXhora) {
+        this.nombre = nombre;
+        this.horasTrabajadas = horasTrabajadas;
+        this.tarifaXhora = tarifaXhora;
+    }
+
+    //Getters/Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public double getTarifaXhora() {
+        return tarifaXhora;
+    }
+
+    public void setTarifaXhora(double tarifaXhora) {
+        this.tarifaXhora = tarifaXhora;
+    }
+
+    //Metodos mostrar
+    @Override
+    public String toString() {
+        return "Empleados{" + "nombre=" + nombre + ", horasTrabajadas=" + horasTrabajadas + ", tarifaXhora=" + tarifaXhora + '}';
+    }
+
+    //Metodos
+    public double calcSueldoBruto() {
+        if (horasTrabajadas <= 40) {
+            return horasTrabajadas * tarifaXhora;
+        } else {
+            int horasExtras = horasTrabajadas - 40;
+            return (40 * tarifaXhora) + (horasExtras * tarifaXhora * 1.5);
+        }
+    }
+
+    public void mostrarDatos() {
+        double sueldo = calcSueldoBruto();
+        System.out.println(nombre + " trabajó " + horasTrabajadas + " horas, cobra " + tarifaXhora + " euros la hora, por lo que le corresponde un sueldo de " + sueldo + " euros.");
+    }
+
 }
--- T7/T8_EJ6/src/t8_ej6/T8_EJ6.java
+++ T7/T8_EJ6/src/t8_ej6/T8_EJ6.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package t8_ej6;
-
+import java.util.Scanner;
 /**
  *
  * @author DAM
@@ -14,7 +14,31 @@ public class T8_EJ6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Introduccion de numero de empleados que se van a "Fichar"
+        Scanner sc = new Scanner(System.in);
+        System.out.print("¿Cuántos empleados desea ingresar? ");
+        int cantidad = sc.nextInt();
+        sc.nextLine();
+        Empleados[] empleados = new Empleados[cantidad];
         
+        //Bucle que crea a los empleados
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("\nEmpleado " + (i + 1));
+            System.out.print("Nombre: ");
+            String nombre = sc.nextLine();
+            System.out.print("Horas trabajadas: ");
+            int horas = sc.nextInt();
+            System.out.print("Tarifa por hora: ");
+            double tarifa = sc.nextDouble();
+            sc.nextLine();
+            empleados[i] = new Empleados(nombre, horas, tarifa);
+        }
+        //Mostrar los empleados creados
+        System.out.println("\n--- Sueldos Brutos ---");
+        for (Empleados e : empleados) {
+            e.mostrarDatos();
+        }
+       
     }
-    
+
 }
