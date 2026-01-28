3d1d06c932e95590122e11f630a49db256f7fbb4
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2026-01-28 18:21:08 +0100

    Cambio de directorio

---
+++ T8/T8_EJ7/src/t8_ej7/T8_EJ7.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t8_ej7;
+
+/**
+ *
+ * @author DAM
+ */
+public class T8_EJ7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
