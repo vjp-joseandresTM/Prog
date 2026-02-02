0e007490704a1dee7289a8c41aff8a02228af312
1DAM - Jose Tarrero Mendez <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2026-02-02 20:48:36 +0100

    Add

---
+++ T8/T8_EJ13-14/src/t8_ej13/pkg14/Alumno.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t8_ej13.pkg14;
+
+/**
+ *
+ * @author alumno
+ */
+public class Alumno {
+    private String nombre;
+    private int edad;
+    private double notaMedia;
+    
+    public Alumno(){
+        this.nombre = "";
+        this.edad = 0;
+        this.notaMedia = 0;
+    }
+    
+    public Alumno(String nombre, int edad, double notaMedia){
+        this.
+    }
+}
