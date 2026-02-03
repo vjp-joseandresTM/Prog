e71d99cea67a7c5d5d074a2c334ae752fd2e4a68
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2026-02-04 00:22:43 +0100

    Añadidos

--- T8/T8_EJ13-14/src/t8_ej13/pkg14/Alumno.java
+++ T8/T8_EJ13-14/src/t8_ej13/pkg14/Alumno.java
@@ -13,6 +13,7 @@ public class Alumno {
     private int edad;
     private double notaMedia;
     
+    //Constructores
     public Alumno(){
         this.nombre = "";
         this.edad = 0;
@@ -20,6 +21,45 @@ public class Alumno {
     }
     
     public Alumno(String nombre, int edad, double notaMedia){
-        this.
+        this.nombre = nombre;
+        this.edad = edad;
+        this.notaMedia = notaMedia;
     }
+
+    //Getters/Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getNotaMedia() {
+        return notaMedia;
+    }
+
+    public void setNotaMedia(double notaMedia) {
+        this.notaMedia = notaMedia;
+    }
+    
+    //Metodos de mostrar
+    @Override
+    public String toString() {
+        return "Alumno{" + "nombre=" + nombre + ", edad=" + edad + ", notaMedia=" + notaMedia + '}';
+    }
+    
+    public  void mostrarAlumno(){
+        System.out.println("Nombre: "+ nombre +
+        " Edad: "+ edad + " Nota media: "+ notaMedia);
+    }
+    
 }
--- T8/T8_EJ13-14/src/t8_ej13/pkg14/T8_EJ1314.java
+++ T8/T8_EJ13-14/src/t8_ej13/pkg14/T8_EJ1314.java
@@ -4,6 +4,8 @@
  */
 package t8_ej13.pkg14;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -15,6 +17,127 @@ public class T8_EJ1314 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        Alumno a1 = new Alumno();
+        a1.setNombre("Tania");
+        a1.setEdad(25);
+        a1.setNotaMedia(7);
+        a1.mostrarAlumno();
+
+        Alumno a2 = new Alumno("Pepe", 19, 8.5);
+        a2.mostrarAlumno();
+
+        //Ampliacion del Ejercicio 14.
+        Alumno[] clase = new Alumno[5];
+        menu(clase);
+
     }
-    
+    //Menu
+    public static void menu(Alumno[] clase) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("\n--- MENÚ ALUMNOS ---");
+            System.out.println("1. Rellenar alumnos");
+            System.out.println("2. Mostrar alumnos");
+            System.out.println("3. Mostrar alumnos por encima de la media");
+            System.out.println("4. Mostrar alumnos con media suspensa");
+            System.out.println("5. Buscar si esta matriculado");
+            System.out.println("6. Salir");
+            System.out.print("Opción: ");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    rellenarAlumno(clase);
+                    break;
+                case 2:
+                    mostrarAlumno(clase);
+                    break;
+                case 3:
+                    mediaPorEncima(clase);
+                    break;
+                case 4:
+                    suspensos(clase);
+                    break;
+                case 5:
+                    buscador(clase);
+                    break;
+                case 6:
+                    System.out.println("Gracias por usar... Adios!");
+                    break;
+                default:
+                    System.out.println("Opción incorrecta");
+            }
+
+        } while (opcion != 6);
+    }
+    //Metodo que rellena el alumno en x posicion
+    public static void rellenarAlumno(Alumno[] clase) {
+        Scanner sc = new Scanner(System.in);
+        int pos;
+
+        do {
+            System.out.print("Introduce posición (0-4): ");
+            pos = sc.nextInt();
+        } while (pos < 0 || pos >= clase.length || clase[pos] != null);
+
+        sc.nextLine();
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("Edad: ");
+        int edad = sc.nextInt();
+        System.out.print("Nota media: ");
+        double nota = sc.nextDouble();
+
+        clase[pos] = new Alumno(nombre, edad, nota);
+        System.out.println("Alumno en posicion: [" + pos + "] creado correctamente");
+    }
+
+    //Mostramos los alumnos RELLENADOS.
+    public static void mostrarAlumno(Alumno[] clase) {
+        for (int i = 0; i < clase.length; i++) {
+            if (clase[i] != null) {
+                clase[i].mostrarAlumno();
+            }
+        }
+    }
+    //Buscamos solo los alumnos q tengan la media por encima de la que introduzcamos
+    public static void mediaPorEncima(Alumno[] clase) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la media que buscas: ");
+        double media = sc.nextDouble();
+
+        for (Alumno al : clase) {
+            if (al != null && al.getNotaMedia() > media) {
+                al.mostrarAlumno();
+            }
+        }
+    }
+    //Mostramos los alumnos que estan suspensos
+    public static void suspensos(Alumno[] clase) {
+        for (Alumno al : clase) {
+            if (al != null && al.getNotaMedia() < 5) {
+                al.mostrarAlumno();
+            }
+        }
+    }
+    //Buscamos los alumnos que estan matriculados.
+    public static void buscador(Alumno[] clase) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el nombre del alumno a buscar: ");
+        String buscarNomb = sc.nextLine();
+        boolean encontrado = false;
+        for (Alumno al : clase) {
+            if (al != null && al.getNombre().equals(buscarNomb)) {
+                encontrado = true;
+            }
+        }
+
+        if (encontrado) {
+            System.out.println("El alumno está matriculado.");
+        } else {
+            System.out.println("El alumno NO está matriculado.");
+        }
+    }
+
 }
