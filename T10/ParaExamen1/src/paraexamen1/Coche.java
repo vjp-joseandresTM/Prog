353775383d18e04180b8e8b7a7c1144f61f96191
vjp-joseandresTM <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2026-03-07 01:45:09 +0100

    EJERCICIO PRACTICA

---
+++ T10/ParaExamen1/src/paraexamen1/Coche.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paraexamen1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM
+ */
+public class Coche {
+
+    private  String matricula;
+    private  String marca;
+    private  int[] revisiones = new int[3];
+
+    public Coche() {
+        this.matricula = "";
+        this.marca = "";
+        this.revisiones = new int[3];
+    }
+
+    public Coche(String matricula, String marca, int[] revisiones) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.revisiones = revisiones;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int[] getRevisiones() {
+        return revisiones;
+    }
+
+    public void setRevisiones(int[] revisiones) {
+        this.revisiones = revisiones;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" + "matricula=" + matricula + ", marca=" + marca + ", revisiones=" + revisiones + '}';
+    }
+
+    public void mostrarInfoCoche() {
+        System.out.println("MARCA: " + marca);
+        System.out.println("MATRICULA: " + matricula);
+        for (int i = 0; i < revisiones.length; i++) {
+            System.out.println("REVISION "+ i +" : " + revisiones[i]);
+        }
+        
+    }
+
+    public int caclMedia() {
+        int suma = 0;
+        for (int i = 0; i < revisiones.length; i++) {
+            suma += revisiones[i];
+        }
+        suma = suma / revisiones.length;
+        return suma;
+    }
+
+    public void introducirInformacion() {
+        Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.print("Introduce matrícula: ");
+        this.matricula = sc.nextLine();
+
+        System.out.print("Introduce marca: ");
+        this.marca = sc.nextLine();
+
+        System.out.println("Introduce las 3 revisiones:");
+        for (int i = 0; i < revisiones.length; i++) {
+            System.out.print("Revisión " + (i + 1) + ": ");
+            revisiones[i] = sc.nextInt();
+        }
+    }
+}
---
+++ T10/ParaExamen1/src/paraexamen1/ParaExamen1.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paraexamen1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM
+ */
+public class ParaExamen1 {
+
+    public final static int FIL = 3;
+    public final static int COL = 4;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Coche[][] aparcamientos = new Coche[FIL][COL];
+        menu(aparcamientos);
+    }
+
+    public static void menu(Coche[][] parking) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("\n **************Menu de gestion del parking*****************");
+            System.out.println("1. Introducir coches");
+            System.out.println("2. Mostrar coches del parking");
+            System.out.println("3. Buscar coche por matricula");
+            System.out.println("4. Mostrar coche con revisiones mas caras");
+            System.out.println("5. Salir del programa");
+
+            System.out.println("\n **********************************************************");
+            System.out.print("\nINTRODUCE UNA OPCION: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    rellenar(parking);
+                    break;
+                case 2:
+                    mostrar(parking);
+                    break;
+                case 3:
+                    buscador(parking);
+                    break;
+                case 4:
+                    reviMascara(parking);
+                    break;
+                case 5:
+
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        } while (opcion != 5);
+    }
+
+    public static void rellenar(Coche[][] parking) {
+        System.out.println("OPCION 1: ");
+        for (int i = 0; i < FIL; i++) {
+            for (int j = 0; j < COL; j++) {
+
+                System.out.println("Introduce la info del coche a rellenar");
+                parking[i][j] = new Coche();
+                parking[i][j].introducirInformacion();
+
+            }
+        }
+    }
+
+    public static void mostrar(Coche[][] parking) {
+        for (int i = 0; i < FIL; i++) {
+            for (int j = 0; j < COL; j++) {
+                if (parking[i][j] != null) {
+                    parking[i][j].mostrarInfoCoche();
+
+                }
+            }
+        }
+        System.out.println("\n");
+    }
+
+    public static void reviMascara(Coche[][] parking) {
+        int caro = 0;
+        for (int i = 0; i < FIL; i++) {
+            for (int j = 0; j < COL; j++) {
+                if (parking[i][j] != null) {
+                    if (caro < parking[i][j].caclMedia()) {
+                        caro = parking[i][j].caclMedia();
+                    }
+                }
+            }
+        }
+        System.out.println("Revision mas cara: " + caro);
+    }
+
+    public static void buscador(Coche[][] parking) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce matrícula a buscar: ");
+        String mat = sc.nextLine();
+
+        int i = 0, j = 0;
+        boolean encontrado = false;
+
+        do {
+            if (parking[i][j] != null && parking[i][j].getMatricula().equalsIgnoreCase(mat)) {
+                System.out.println("Coche encontrado:");
+                System.out.println("Fila: " + i);
+                System.out.println("Columna: " + j);
+                System.out.println("Marca: " + parking[i][j].getMarca());
+                encontrado = true;
+            }
+
+            
+            j++;
+            if (j == COL) {
+                j = 0;
+                i++;
+            }
+
+        } while (!encontrado && i < FIL);
+
+        if (!encontrado) {
+            System.out.println("No existe ningún coche con esa matricula.");
+        }
+    }
+
+}
