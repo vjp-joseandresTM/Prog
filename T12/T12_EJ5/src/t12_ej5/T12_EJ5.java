5d924e3af8744d69bbc00160e19dab782f4a1fca
1DAM - Jose Tarrero Mendez <jatarrerom01@informatica.iesvalledeljerteplasencia.es>
2026-04-13 20:47:30 +0200

    ADD

---
+++ T12/T12_EJ5/src/t12_ej5/T12_EJ5.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t12_ej5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class T12_EJ5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void menu(){
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        
+        do {            
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        } while (opcion != 3);
+    }
+    
+}
